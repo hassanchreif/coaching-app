51ff78c354a5f64907eb8a670eec664c169535b3
hassan <202212004@ua.edu.lb>
2026-05-18 14:13:27 +0300

    regestration

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(data: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: data.email,
+        password: hashedPassword,
+        name: data.name,
+        role: data.role,
+      },
+    });
+
+    return {
+      message: 'User created successfully',
+      user,
+    };
+  }
+}
