3ff8c204a86f885c0c93c157957b105eaec0865d
hassan <202212004@ua.edu.lb>
2026-05-18 14:25:43 +0300

    login

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,8 +3,28 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule,
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+
+        signOptions: {
+          expiresIn: '7d',
+        },
+      }),
+    }),
+  ],
+
   controllers: [AuthController],
   providers: [AuthService],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,14 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
 import { PrismaService } from '../prisma/prisma.service';
+
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+
 import * as bcrypt from 'bcrypt';
+
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(data: RegisterDto) {
-    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (existingUser) {
+      throw new UnauthorizedException(
+        'User already exists',
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(
+      data.password,
+      10,
+    );
 
     const user = await this.prisma.user.create({
       data: {
@@ -21,6 +48,42 @@ export class AuthService {
 
     return {
       message: 'User created successfully',
+      user,
+    };
+  }
+
+  async login(data: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException(
+        'Invalid credentials',
+      );
+    }
+
+    const passwordMatches = await bcrypt.compare(
+      data.password,
+      user.password,
+    );
+
+    if (!passwordMatches) {
+      throw new UnauthorizedException(
+        'Invalid credentials',
+      );
+    }
+
+    const token = await this.jwtService.signAsync({
+      sub: user.id,
+      email: user.email,
+      role: user.role,
+    });
+
+    return {
+      access_token: token,
       user,
     };
   }
